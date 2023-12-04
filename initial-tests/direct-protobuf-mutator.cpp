a6c3fa56c9b03f98c866cd67b38da5d92fc90af4
cymtrick <varma.prashanth@hotmail.com>
2023-12-04 17:51:02 +0000

    testing mutator

--- initial-tests/direct-protobuf-mutator.cpp
+++ initial-tests/direct-protobuf-mutator.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
 #include "libprotobuf-mutator/src/mutator.h"
 #include "libfuzzstruct.h"
-#include <cstdint>
-#include <cstddef>
-#include <cstring>  
-#include <cstdlib>  
-#include <iostream>
-#include "test-struct.pb.h" 
-
+#include "test-struct.pb.h"
 
 struct testStruct {
-  int32_t IntValue;
-  char CharValue;
-  float FloatValue;
+  teststruct::testStruct protoMessage;
+
+  std::string& CharValue() { return *protoMessage.mutable_charvalue(); }
 };
 
 void mutateStruct(testStruct* sample) {
-  // Convert SampleStruct to protobuf message
-  teststruct::testStruct protoMessage;
-  // Use protobuf_mutator::Mutator to mutate the protobuf message
-  protobuf_mutator::Mutator mutator;
-  mutator.Mutate(&protoMessage, protoMessage.ByteSizeLong());
+  
+  std::cout << "Before mutation: " << sample->protoMessage.DebugString() << std::endl;
 
-  // Convert the mutated protobuf message back to SampleStruct
-  sample->IntValue = protoMessage.intvalue();
-  sample->CharValue = protoMessage.charvalue();
-  sample->FloatValue = protoMessage.floatvalue();
+  
+  protobuf_mutator::Mutator mutator;
+  mutator.Seed(123);
+  mutator.Mutate(&sample->protoMessage, 200);
+
+  
+  std::cout << "After mutation: " << sample->protoMessage.DebugString() << std::endl;
 }
 
 int main() {
