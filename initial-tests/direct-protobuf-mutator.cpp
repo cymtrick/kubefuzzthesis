8a9629c4fa52b3885933ba6990815029107577a7
cymtrick <varma.prashanth@hotmail.com>
2023-12-05 18:49:05 +0000

    testing DEFINE_PROTO_FUZZER

--- initial-tests/direct-protobuf-mutator.cpp
+++ initial-tests/direct-protobuf-mutator.cpp
@@ -2,21 +2,28 @@
 #include "libprotobuf-mutator/src/mutator.h"
 #include "libfuzzstruct.h"
 #include "test-struct.pb.h"
+#include "libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.h"
 
 struct testStruct {
   char charValue=2;
   
 };
 
+DEFINE_PROTO_FUZZER(const teststruct::test& input) {
+  // Code which needs to be fuzzed.
+  std::cout << "Mutated Message Before: " << input.charvalue() << std::endl;
+}
 void mutateStruct(const uint8_t* data, size_t size) {
   protobuf_mutator::Mutator mutator;
   teststruct::test setcharvalue;
   setcharvalue.ParseFromArray(data, static_cast<int>(size));
-  std::cout << "Mutated Message Before: " << setcharvalue.charvalue() << std::endl;
+  
+  
   mutator.Seed(99213129);
-  mutator.Mutate(&setcharvalue, 9921);
+  mutator.Fix(&setcharvalue);
+  mutator.Mutate(&setcharvalue, setcharvalue.ByteSizeLong());
   // std::string mutatedData = getmessage->SerializeAsString();
-  std::cout << "Mutated Message After: " << setcharvalue.SerializeAsString() << std::endl;
+  std::cout << "Mutated Message After: " << setcharvalue.charvalue() << std::endl;
   
 }
 
@@ -26,7 +33,7 @@ int main() {
   teststruct::test::Message* getmessage;
 
   for (int i = 37; i < 200; ++i) {
-      charvalue.set_charvalue("sadasd");
+      charvalue.set_charvalue("sadasd"+std::to_string(i));
       std::string data = charvalue.SerializeAsString();  
       
       mutateStruct(reinterpret_cast<const uint8_t*>(data.data()), data.size());
@@ -36,3 +43,5 @@ int main() {
 
   return 0;
 }
+
+//g++ -o protomutator -L/usr/local/lib/ -I/usr/local/include/libprotobuf-mutator -L-I../libs/libprotobuf-mutator/src/libfuzzer/ -L. -I. direct-protobuf-mutator.cpp test-struct.pb.cc  -lfuzzstruct -lpthread -lprotobuf-mutator -lprotobuf 
