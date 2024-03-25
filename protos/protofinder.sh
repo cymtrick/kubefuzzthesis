a574fe1047a752cad4a174c77b7dd23c5de41835
cymtrick <varma.prashanth@hotmail.com>
2024-03-25 21:59:30 +0000

    protos file structure

--- protos/protofinder.sh
+++ protos/protofinder.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 source_dir="../libs/kubernetes"
 destination_dir="./proto_defs"
+
+# Create the destination directory if it doesn't exist
 mkdir -p "$destination_dir"
 
-proto_files=$(find "$source_dir" -type f -name "*.proto")
-for file in $proto_files; do
-    cp "$file" "$destination_dir"
+find "$source_dir" -type f -name "*.proto" | while read -r file; do
+    relative_path="${file#$source_dir/}"
+    mkdir -p "$destination_dir/$(dirname "$relative_path")"
+    cp "$file" "$destination_dir/$relative_path"
 done
 
 echo "Proto files copied successfully to $destination_dir"
